564349f12d73c938091cbe96a2c5eb1cce24f02f
Mariem Ayadi <mayadi@smith.edu>
2015-11-01 10:46:31 -0500

    Group prototype in action

---
+++ friend.me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  friend.me
+//
+//  Created by Mariem Ayadi on 11/1/15.
+//  Copyright © 2015 Mariem Ayadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
